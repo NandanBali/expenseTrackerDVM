12d9935799cc492504c5ad8758f989df86f672bf
NandanBali <f20240380@pilani.bits-pilani.ac.in>
2024-09-22 22:55:17 +0530

    new Card

--- lib/views/CustomCards2.dart
+++ lib/views/CustomCards2.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, unnecessary_this
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:expensetracker/models/Payment.dart';
@@ -31,106 +31,99 @@ class HistoryCard extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Column(
+      child: Row(
         children: <Widget>[
           SizedBox(
-            width: double.infinity,
-            height: 24.0,
+            height: 100.0,
           ),
-          Text(
-            "₹${this.payment.txAmount.toString()}",
-            style: TextStyle(
-              fontFamily: 'GSans',
-              color: Colors.white,
-              fontWeight: FontWeight.w600,
-              fontSize: 48.0,
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Align(
-              alignment: Alignment.bottomLeft,
-              child: Text(
-                "${this.payment.txTime.day}/${this.payment.txTime.month}/${this.payment.txTime.year}",
-                style: TextStyle(
-                  fontFamily: "GSans",
-                  color: Colors.white,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 16.0,
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "₹${this.payment.txAmount.toString()}",
+                  style: TextStyle(
+                    fontFamily: 'GSans',
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 40.0,
+                  ),
                 ),
-              )),
-          SizedBox(
-            height: 20.0,
-          ),
-          Container(
-            width: double.infinity,
-            child: OutlinedButton(
-              onPressed: () {
-                showModalBottomSheet(
-                    context: context,
-                    shape: const RoundedRectangleBorder(
-                      // <-- SEE HERE
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(16.0),
+                SizedBox(
+                  height: 12.0,
+                ),
+                Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      "On ${this.payment.txTime.day}/${this.payment.txTime.month}/${this.payment.txTime.year}",
+                      style: TextStyle(
+                        fontFamily: "GSans",
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16.0,
                       ),
-                    ),
-                    builder: (context) {
-                      return SizedBox(
-                        width: double.infinity,
-                        height: 200,
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Text(
-                                "Amount: ${this.payment.txAmount.toString()}",
-                                style: TextStyle(
-                                  fontFamily: "GSans",
-                                  fontSize: 16.0,
-                                ),
-                              ),
-                              Text(
-                                "Time: ${this.payment.txTime.hour}:${this.payment.txTime.minute} on ${this.payment.txTime.day}/${this.payment.txTime.month}/${this.payment.txTime.year}",
-                                style: TextStyle(
-                                  fontFamily: "GSans",
-                                  fontSize: 16.0,
-                                ),
-                              ),
-                              Text(
-                                "Description: ${this.payment.txDesc}",
-                                style: TextStyle(
-                                  fontFamily: "GSans",
-                                  fontSize: 16.0,
-                                ),
-                              ),
-                              Text(
-                                "Category: ${this.payment.category}",
-                                style: TextStyle(
-                                  fontFamily: "GSans",
-                                  fontSize: 16.0,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    });
-              },
-              style: ButtonStyle(
-                foregroundColor: WidgetStatePropertyAll(Colors.white),
-                shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                    side: BorderSide(color: Colors.white))),
-              ),
-              child: Text(
-                'More Details',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-              ),
+                    )),
+              ],
             ),
-          )
+          ),
+          OutlinedButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(16.0),
+                    ),
+                  ),
+                  builder: (context) {
+                    return SizedBox(
+                      width: double.infinity,
+                      height: 200,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              "Amount: ${this.payment.txAmount.toString()}",
+                              style: TextStyle(
+                                fontFamily: "GSans",
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            Text(
+                              "Time: ${this.payment.txTime.hour}:${this.payment.txTime.minute} on ${this.payment.txTime.day}/${this.payment.txTime.month}/${this.payment.txTime.year}",
+                              style: TextStyle(
+                                fontFamily: "GSans",
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            Text(
+                              "Description: ${this.payment.txDesc}",
+                              style: TextStyle(
+                                fontFamily: "GSans",
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            Text(
+                              "Category: ${this.payment.category}",
+                              style: TextStyle(
+                                fontFamily: "GSans",
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            },
+            child: Icon(
+              Icons.info_outline,
+              size: 20.0,
+            ),
+          ),
         ],
       ),
     );
